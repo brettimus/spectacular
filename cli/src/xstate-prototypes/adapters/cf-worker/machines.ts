7b0690b9ecb6df0b13a2d1897e369cc641ed1630
Brett Beutell <brbeut@gmail.com>
2025-04-04 23:10:23 -0700

    Update comments

--- cli/src/xstate-prototypes/adapters/cf-worker/machines.ts
+++ cli/src/xstate-prototypes/adapters/cf-worker/machines.ts
@@ -13,6 +13,15 @@ export const cliChatMachine = chatMachine.provide({
   actors: {
     saveSpec: saveSpecToApiActor,
   },
+  actions: {
+    handleStreamChunk: (_, _params) => {
+      // NOOP - But this is a good place to add logic to handle chunks of text from the streaming response
+    },
+    handleNewAssistantMessages: (_, _params) => {
+      // NOOP - But this is a good place to add logic to handle new assistant messages after the streaming response has completed
+      //        BUT, this will NOT fire after a spec is generated. for that, rely on the `saveSpec` actor.
+    },
+  },
 });
 
 export const cliSchemaCodegenMachine = schemaCodegenMachine.provide({
