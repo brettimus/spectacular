975c3503b06901104bc1426f2c99eb39af544bd0
Brett Beutell <brbeut@gmail.com>
2025-04-06 11:57:27 -0700

    Update router to use gpt-4o

--- cli/src/xstate-prototypes/ai/chat/router/openai.ts
+++ cli/src/xstate-prototypes/ai/chat/router/openai.ts
@@ -1,5 +1,6 @@
 export const OPENAI_STRATEGY = {
-  modelName: "gpt-4o-mini",
+  // NOTE - I was using gpt-4o-mini, but it was getting confused about the user's intent a lot
+  modelName: "gpt-4o",
   modelProvider: "openai",
   temperature: 0.2,
   getSystemPrompt,
@@ -12,11 +13,16 @@ The user has approached you with an idea for a software project.
 
 Look at the conversation history and determine what we need to do next.
 
-Either we have sufficient information to generate an implementation plan, or we need to ask the user a follow-up question.
+Either we have sufficient information to generate an implementation plan for the API, or we need to ask the user a follow-up question.
 
 Consider the user's intent, as well as the following:
 
 - Do we have a clear idea of the domain of the project?
 - Have we asked about user authentication yet? We should always ask about auth, just to be sure, unless it's obvious that the user doesn't need it.
-- Do we have an idea of features like auth, email, realtime, etc?`;
+- Do we have an idea of features like auth, email, realtime, etc?
+
+Use the tools provided to you. Respond in JSON format.
+
+DO NOT CREATE AN IMPLEMENTATION PLAN UNLESS YOU HAVE ALL THE INFORMATION YOU NEED, OR THE USER TELLS YOU TO DO SO.
+`;
 }
