fbbe90dc65f1d3f6ed9aa2b4b84823a51e992d2e
Brett Beutell <brbeut@gmail.com>
2025-04-14 17:27:46 +0200

    Remove script for testing markdown imports

--- cli/src/xstate-prototypes/knowledge-test.ts
+++
@@ -1,6 +0,0 @@
-// Import a markdown file as raw text
-import testMarkdown from "./test-content.md?raw";
-
-console.log("First 100 characters of the imported markdown:");
-console.log(testMarkdown.substring(0, 100));
-console.log("\nTotal markdown length:", testMarkdown.length);
